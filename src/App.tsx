c7031285e5aebf29ba4f5f8893ad256aa7ad3cf3
Sudhir7832 <73008445+Sudhir7832@users.noreply.github.com>
2025-03-06 22:12:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Payment from "./pages/Payment";
+
 
 const queryClient = new QueryClient();
 
